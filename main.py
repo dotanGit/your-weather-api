df8839ae5aab8fa58bc35699a0bcb135f80ef9cf
dotan <frenkeldotan1998@gmail.com>
2023-04-14 10:04:18 +0300

    Add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 app = Flask(__name__)
 
 stations = pd.read_csv("data-jupyter-notebook/data_small/stations.txt", skiprows=17)
-stations = stations[["STAID","STANAME                                 "]]
+stations = stations[["STAID", "STANAME                                 "]]
 
 @app.route("/")
 def home():   # when a visitor clicks home the function is called
@@ -18,6 +18,28 @@ def about(station, date):   # when a visitor clicks home the function is called
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data-jupyter-notebook/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')   # orient='records' change the format of the dictionary
+    return result
+
+@app.route("/api/v1/<station>")
+def all_data_station(station):
+    filename = "data-jupyter-notebook/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')   # orient='records' change the format of the dictionary
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def all_data_station_year(station, year):
+    filename = "data-jupyter-notebook/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient='records')
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
